10ae3f1a054c63f2400a4b5408f28153831be0d3
cplacier8 <colin.placier@etudiant.univ-lr.fr>
2020-04-05 11:52:10 +0200

    Lien du bouton commencer dans l'activité "Theme"

--- app/src/main/java/com/example/blindtest/ThemeActivity.java
+++ app/src/main/java/com/example/blindtest/ThemeActivity.java
@@ -2,10 +2,15 @@ package com.example.blindtest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ThemeActivity extends AppCompatActivity {
+
+    Button btnBegin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +22,18 @@ public class ThemeActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_theme);
+
+        init();
+    }
+
+    private void init(){
+        btnBegin = findViewById(R.id.id_begin);
+        btnBegin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), SingleplayerActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
